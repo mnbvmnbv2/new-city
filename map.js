89454d371c79946c9897266dec30b48f835c5f04
mnbvmnbv2 <32966421+mnbvmnbv2@users.noreply.github.com>
2020-02-01 00:23:52 +0100

    .

--- map.js
+++ map.js
@@ -1,20 +1,35 @@
 //-------------------MAP-OG-OVERLAY------------------------
-class NatureClass {
-	constructor(mapTile, type, food, wood, stone, gold, name) {
-		this.mapTile = mapTile;
-		this.type = type;
-		this.food = food;
-		this.wood = wood;
-		this.stone = stone;
-		this.gold = gold;
-		this.name = name;
+class TileClass {
+	constructor(tileNumber, heigth) {
+		this.tileNumber = tileNumber;
+
+		if (heigth > maxHeigth){
+			this.height = maxHeigth;
+		} else if(heigth < minHeigth){
+			this.height = minHeigth;
+		} else{
+			this.height = heigth;
+		}
+		
+		if(this.height <= 0){
+			this.type = 'desert';
+		} else{
+			this.type = 'sea';
+		}
+		
+		this.food = 2;
+		this.wood = 2;
+		this.stone = 2;
+		this.value = 2;
+		this.name = "generic";
 	}
 }
 
+//---------MAPSIZE---------------
 const boxSize = Math.floor(screen.width/50);
 const mapWidth = Math.floor(screen.width/boxSize)-5;
-console.log(mapWidth);
 const mapHeight = 20;
+//-------------------------------
 
 const gameEl = document.getElementById('game');
 const overlayEl = document.getElementById('overlay');
@@ -31,78 +46,58 @@ infoEl.style.width = `${boxSize * mapWidth}px`;
 infoEl.style.marginTop = `${boxSize * mapHeight}px`;
 infoEl.style.height = `${boxSize * mapHeight/2}px`;
 
-const tileName = [ 'sea', 'desert' ];
+//--------------MAPMAKER------------------------
+
+const maxHeigth = 8;
+const minHeigth = -8;
+const heightRange = maxHeigth-minHeigth;
+const newHeigthRange = 8; // 8 = (-4)-4
+const heigthToOver = 4;
+
 //const mapTypeChances = [ 1, 2 ];
 // const mapTotalChance = mapTypeChances.reduce();
 
-//const tileFuncs = [desertMaker()]
-
-let nature = [];
 let map = [];
-let heightMap = [];
-
-createHeightMap();
-function createHeightMap() {
-	for (var i = 0; i < mapHeight; i++) {
-		line = [];
-		for (var j = 0; j < mapWidth; j++) {
-			if (i == 0 && j == 0) {
-				line.push(5);
-			} else if (j == 0) {
-				line.push(Math.floor(Math.random() * 9) + 1);
-			} else {
-				let randomHeigth = Math.floor(Math.random() * 5);
-				if (line[j - 1] + randomHeigth - 4 <= 0) {
-					line.push(1);
-				} else if (line[j - 1] + randomHeigth - 2 >= 10) {
-					line.push(9);
-				} else {
-					line.push(line[j - 1] + randomHeigth - 2);
-				}
-				if (i != 0) {
-					if (line[j] < heightMap[i - 1][j] - 3) {
-						line[j] = heightMap[i - 1][j] - 3;
-					} else if (line[j] > heightMap[i - 1][j] + 3) {
-						line[j] = heightMap[i - 1][j] + 3;
-					}
-				}
-			}
-		}
-		heightMap.push(line);
-	}
-}
 
 createMap();
 function createMap() {
-	let makingMapTile = 0;
-
-	for (var i = 0; i < heightMap.length; i++) {
-		let mapLine = [];
-		for (var j = 0; j < heightMap[i].length; j++) {
-			let mapTileType;
-			if (heightMap[i][j] < 4) {
-				mapTileType = 0;
+	let tileCounter = 0; 
+	for (var i = 0; i < mapHeight; i++) { //i per line
+		line = [];
+		for (var j = 0; j < mapWidth; j++) {
+			if (i == 0 && j == 0) { 
+				//first tile
+				let o = new TileClass(tileCounter,0); // first tile with heigth 0
+				line.push(o);
+			} else if (j == 0) {
+				//first tile on new line
+				let o = new TileClass(tileCounter,Math.floor(Math.random() * 4) - 2); //random heigth (-2)-1
+				line.push(o);
 			} else {
-				mapTileType = 1;
+				let o = new TileClass(tileCounter,line[j-1].height + Math.floor(Math.random() * newHeigthRange*+1)-newHeigthRange/2); // (-4)-4 range
+				line.push(o)
+				}
+			if (i != 0) {
+				//compares to line over
+				if (line[j].height < map[i - 1][j].height - heigthToOver) {
+					line[j].height = map[i - 1][j].height - heigthToOver;
+				} else if (line[j] > map[i - 1][j] + heigthToOver) {
+					line[j] = map[i - 1][j] + heigthToOver;
+				}
 			}
-			mapLine.push(mapTileType);
-			if (mapTileType === 0) {
-				seaMaker(makingMapTile); // nature/...
-			} else if (mapTileType === 1) {
-				desertMaker(makingMapTile); // nature/...
-			}
-			makingMapTile++;
+			tileCounter++;
 		}
-		map.push(mapLine);
+		map.push(line);
 	}
 }
+
 
 for (var i = 0; i < mapWidth * mapHeight; i++) {
 	var clickable = document.createElement('div');
 	clickable.classList.add('overlayBlock');
 	clickable.id = i;
 	clickable.addEventListener('click', selected);
-	clickable.style.opacity = 0.1;
+	//clickable.style.opacity = 0.1;
 	overlayEl.appendChild(clickable);
 }
 var overlayblocks = document.getElementsByClassName('overlayBlock');
@@ -121,10 +116,10 @@ var modal = document.getElementById("myModal");
 const spantextEl = document.getElementById("spantext");
 
 // Get the <span> element that closes the modal
-var span = document.getElementById("span1");
+var closeBtnEl = document.getElementById("close");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeBtnEl.onclick = function() {
   modal.style.display = "none";
 }
 
@@ -137,10 +132,17 @@ window.onclick = function(event) {
 
 //--------------------------------------------------------------
 
+function findTile(tile){
+	let i = Math.floor(tile/mapWidth);
+	let j = tile - (mapWidth*i)
+	return map[i][j];
+}
+
 function selected(e){
-	//modal.style.display = "block";
-	spantextEl.innerHTML = "Dette er block " + e.target.id;
-	let line = 0;
+	modal.style.display = "block";
+	console.log(findTile(e.target.id))
+	spantextEl.innerHTML = `Dette er block ${e.target.id}<br>Denne har: ${findTile(e.target.id).value} verdi`;
+
 	let pointer = 0;
 	for(var i = 0; i < Number(e.target.id); i++){
 		pointer++;
@@ -149,14 +151,14 @@ function selected(e){
 			pointer = 0;
 		}
 	}
-	if(map[line][pointer] == 0){
+
+	e.target.innerHTML = '1';
+
+	/*if(map[line][pointer] == 0){
 		map[line][pointer] = 1;
 	} else{
 		map[line][pointer] = 0;
-	}
-	console.log(line);
-	console.log(pointer);
-	console.log(e.target.id)
+	}*/
 }
 
 //--------------------TILES-------------------------------
@@ -178,7 +180,7 @@ function drawGame() {
 		for (var j = 0; j < map[i].length; j++) {
 			tilePic = document.createElement('img');
 
-			tilePic.src = `bilder/${tileName[map[i][j]]}/0.png`;
+			tilePic.src = `bilder/${map[i][j].type}/0.png`;
 
 			ctx.drawImage(tilePic, x, y*7/5, boxSize, boxSize*7/5);
 
